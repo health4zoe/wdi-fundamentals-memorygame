66b8fcd179473e83e67d27f2893354799bc4a95f
Dee Davis <threedaycleanse@gmail.com>
2017-02-11 10:16:01 -0500

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,19 @@
 console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+if (cardThree && cardFour) {
+  alert("You found a match!");
+} else if (cardTwo && cardOne) {
+  alert("You found a match");
+} else if (cardFour && cardTwo) {
+  alert("Sorry try again!");
+} else if (cardFour && cardOne) {
+  alert("Sorry try again!");
+} else if (cardThree && cardTwo) {
+  alert("Sorry try again!");
+} else if (cardOne && cardThree) {
+  alert("Sorry try again!");
+}
